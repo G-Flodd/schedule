dd2399a6caabe55a63f12b49718f261599d4bc50
G-Flodd <georgedodd@hotmail.co.uk>
2023-01-27 21:00:57 +0000

    Final Updates

--- challenge (4)/starter/script.js
+++ challenge (4)/starter/script.js
@@ -8,6 +8,9 @@ var update = function () {
 
 var textRow = $(".textRow")
 
+var preSave = JSON.parse(localStorage.getItem("memSave"));
+
+
 $(document).ready(function () {
     datetime = $('#currentDay')
     update();
@@ -16,34 +19,38 @@ $(document).ready(function () {
 
 var hour = moment().hour();
 
-function timeChecker(hour, timeBlock) {
-    var currentHour = moment().hours()
-    if (currentHour === hour) {
-        timeBlock.addClass('present')
-        console.log('You are in the Present!')
-    } else if (currentHour > hour) {
-        console.log(' <- Elements in the Past!')
-        timeBlock.addClass('past')
-        timeBlock.removeClass('present')
-    } else {
-        console.log('<- Elements in the Future!')
-        timeBlock.addClass('future')
-    }
-} 
-
 for (let i = 0; i < textRow.length; i++) {
- 
+
     var blockID = textRow[i].id;
-    
-    if(blockID == hour){
+
+    if (blockID == hour) {
         textRow[i].classList.add('present')
     }
-    else if(blockID < hour){
+    else if (blockID < hour) {
         textRow[i].classList.add('past')
     }
-    else if(blockID > hour){
+    else if (blockID > hour) {
         textRow[i].classList.add('future')
     }
-
-
 }
+
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var textEntry = $(this).siblings("textarea").val();
+    var time = $(this).siblings("textarea").attr('id');
+    localStorage.setItem(time, textEntry);
+});
+
+$(function () {
+    document.getElementById("9").value = localStorage.getItem("9");
+    document.getElementById("10").value = localStorage.getItem("10");
+    document.getElementById("11").value = localStorage.getItem("11");
+    document.getElementById("12").value = localStorage.getItem("12");
+    document.getElementById("13").value = localStorage.getItem("13");
+    document.getElementById("14").value = localStorage.getItem("14");
+    document.getElementById("15").value = localStorage.getItem("15");
+    document.getElementById("16").value = localStorage.getItem("16");
+    document.getElementById("17").value = localStorage.getItem("17");  
+})
